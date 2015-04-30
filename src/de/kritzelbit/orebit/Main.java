14ee87aad34b583faca7780ba13dc75b53134035
bkis <boerge.kiss@gmail.com>
2015-04-30 15:45:29 +0200

    init test scene

--- src/de/kritzelbit/orebit/Main.java
+++ src/de/kritzelbit/orebit/Main.java
@@ -1,34 +1,61 @@
 package de.kritzelbit.orebit;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.light.AmbientLight;
+import com.jme3.light.DirectionalLight;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
-import com.jme3.scene.shape.Box;
+import com.jme3.scene.shape.Sphere;
+import com.jme3.system.AppSettings;
 
-/**
- * test
- * @author normenhansen
- */
+
 public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
         Main app = new Main();
+        
+        //configure settings
+        AppSettings settings = new AppSettings(true);
+        settings.setResolution(1024, 768);
+        settings.setMinResolution(1024, 768);
+        settings.setVSync(true);
+        settings.setFullscreen(false);
+        settings.setTitle("Ore Bit");
+        
+        app.showSettings = false;
+        app.setSettings(settings);
         app.start();
     }
 
     @Override
     public void simpleInitApp() {
-        Box b = new Box(1, 1, 1);
-        Geometry geom = new Geometry("Box", b);
+        
+        //test cam settings
+        
 
-        Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geom.setMaterial(mat);
-
-        rootNode.attachChild(geom);
+        //test sphere
+        Sphere s = new Sphere(32, 32, 2);
+        Geometry sphere = new Geometry("Sphere", s);
+        Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
+        mat.setColor("Diffuse", ColorRGBA.Blue);
+        mat.setColor("Ambient", ColorRGBA.Blue);
+        mat.setColor("Specular", ColorRGBA.Blue);
+        sphere.setMaterial(mat);
+        rootNode.attachChild(sphere);
+        
+        //ambient light
+        AmbientLight ambient = new AmbientLight();
+        ambient.setColor(ColorRGBA.White);
+        rootNode.addLight(ambient); 
+        
+        //sunlight
+        DirectionalLight sun = new DirectionalLight();
+        sun.setDirection((new Vector3f(-0.5f, -0.5f, -0.5f)).normalizeLocal());
+        sun.setColor(ColorRGBA.White);
+        rootNode.addLight(sun); 
     }
 
     @Override
