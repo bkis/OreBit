b32cd81a3406c96d48122d3777afbb50fd5db776
kritzelbit <boerge.kiss@gmail.com>
2015-05-01 16:58:15 +0200

    orbit physics prototype

---
+++ src/de/kritzelbit/orebit/GravityControl.java
@@ -0,0 +1,79 @@
+package de.kritzelbit.orebit;
+
+import com.jme3.export.InputCapsule;
+import com.jme3.export.JmeExporter;
+import com.jme3.export.JmeImporter;
+import com.jme3.export.OutputCapsule;
+import com.jme3.math.FastMath;
+import com.jme3.math.Vector3f;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Spatial;
+import com.jme3.scene.control.AbstractControl;
+import com.jme3.scene.control.Control;
+import com.jme3.scene.shape.Sphere;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ *
+ * @author boss
+ */
+public class GravityControl extends AbstractControl {
+    
+    private Vector3f velocity;
+    
+    public GravityControl(Vector3f initialVelocity){
+        velocity = initialVelocity;
+    }
+
+    public void applyForce(List<Geometry> sources){
+        for (Geometry geom : sources){
+            if (geom == spatial) return;
+            float distance = geom.getWorldTranslation().distance(spatial.getWorldTranslation());
+            float force = ((Sphere)geom.getMesh()).getRadius();
+            Vector3f direction = geom.getWorldTranslation().subtract(spatial.getWorldTranslation());
+            velocity = velocity.add(direction.divide(FastMath.pow(distance, 2)).mult(force*10));
+        }
+    }
+
+    @Override
+    protected void controlUpdate(float tpf) {
+        spatial.move(velocity.mult(tpf/10));
+        
+        //correct z-Axis
+        spatial.setLocalTranslation(
+                spatial.getLocalTranslation().x,
+                spatial.getLocalTranslation().y,
+                0);
+    }
+    
+    @Override
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+        //Only needed for rendering-related operations,
+        //not called when spatial is culled.
+    }
+    
+    public Control cloneForSpatial(Spatial spatial) {
+        GravityControl control = new GravityControl(velocity);
+        //TODO: copy parameters to new Control
+        return control;
+    }
+    
+    @Override
+    public void read(JmeImporter im) throws IOException {
+        super.read(im);
+        InputCapsule in = im.getCapsule(this);
+        //TODO: load properties of this Control, e.g.
+        //this.value = in.readFloat("name", defaultValue);
+    }
+    
+    @Override
+    public void write(JmeExporter ex) throws IOException {
+        super.write(ex);
+        OutputCapsule out = ex.getCapsule(this);
+        //TODO: save properties of this Control, e.g.
+        //out.write(this.value, "name", defaultValue);
+    }
+}
--- src/de/kritzelbit/orebit/Main.java
+++ src/de/kritzelbit/orebit/Main.java
@@ -10,9 +10,13 @@ import com.jme3.renderer.RenderManager;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Sphere;
 import com.jme3.system.AppSettings;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Main extends SimpleApplication {
+    
+    private List<Geometry> geoms;
 
     public static void main(String[] args) {
         Main app = new Main();
@@ -29,22 +33,47 @@ public class Main extends SimpleApplication {
         app.setSettings(settings);
         app.start();
     }
-
+    
+    
     @Override
     public void simpleInitApp() {
         
         //test cam settings
+        flyCam.setEnabled(false);
+        cam.setLocation(new Vector3f(0,0,100));
         
+        geoms = new ArrayList<Geometry>();
 
-        //test sphere
-        Sphere s = new Sphere(32, 32, 2);
+        //test sphere planet
+        Sphere s = new Sphere(32, 32, 1);
         Geometry sphere = new Geometry("Sphere", s);
         Material mat = new Material(assetManager, "Common/MatDefs/Light/Lighting.j3md");
         mat.setColor("Diffuse", ColorRGBA.Blue);
         mat.setColor("Ambient", ColorRGBA.Blue);
         mat.setColor("Specular", ColorRGBA.Blue);
         sphere.setMaterial(mat);
+        sphere.addControl(new GravityControl(new Vector3f(0,-40,0)));
         rootNode.attachChild(sphere);
+        geoms.add(sphere);
+        
+        //test sphere object
+        Sphere o = new Sphere(32, 32, 1);
+        Geometry obj = new Geometry("Sphere", o);
+        obj.setMaterial(mat);
+        obj.addControl(new GravityControl(new Vector3f(0,40,0)));
+        obj.move(10, -20, 0);
+        rootNode.attachChild(obj);
+        geoms.add(obj);
+        
+        //test sphere object
+        Sphere p = new Sphere(32, 32, 1);
+        Geometry pl = new Geometry("Sphere", p);
+        pl.setMaterial(mat);
+        pl.addControl(new GravityControl(Vector3f.ZERO));
+        pl.move(20, -20, 0);
+        rootNode.attachChild(pl);
+        geoms.add(pl);
+        
         
         //ambient light
         AmbientLight ambient = new AmbientLight();
@@ -60,7 +89,8 @@ public class Main extends SimpleApplication {
 
     @Override
     public void simpleUpdate(float tpf) {
-        //TODO: add update code
+        for (Geometry geom : geoms)
+            geom.getControl(GravityControl.class).applyForce(geoms);
     }
 
     @Override
