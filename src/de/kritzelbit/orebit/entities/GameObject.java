aa9f8cb5d4b090b41453a33e5db2cfa95a3138b1
kritzelbit <boerge.kiss@gmail.com>
2015-05-06 22:01:50 +0200

    GameObject update

--- src/de/kritzelbit/orebit/entities/GameObject.java
+++ src/de/kritzelbit/orebit/entities/GameObject.java
@@ -1,21 +1,36 @@
 
 package de.kritzelbit.orebit.entities;
 
+import com.jme3.bullet.control.RigidBodyControl;
+import com.jme3.math.Vector3f;
 import com.jme3.scene.Spatial;
 
 
 public abstract class GameObject {
     
-    private Spatial spatial;
-    private String name;
+    protected Spatial spatial;
+    protected String name;
+    protected RigidBodyControl physics;
     
-    public GameObject(String name, Spatial spatial){
+    public GameObject(String name, Spatial spatial, RigidBodyControl physics){
         this.name = name;
         this.spatial = spatial;
     }
     
     public Spatial getSpatial(){
         return spatial;
+    }
+    
+    public float getX(){
+        return physics.getPhysicsLocation().x;
+    }
+    
+    public float getY(){
+        return physics.getPhysicsLocation().y;
+    }
+    
+    public void setLocation(float x, float y){
+        physics.setPhysicsLocation(new Vector3f(x ,y ,0));
     }
     
 }
