04e9bc8186c13eb698f3d483a129c8cc597ee768
kritzelbit <boerge.kiss@gmail.com>
2015-05-18 15:28:43 +0200

    notes

--- src/de/kritzelbit/orebit/io/GameIO.java
+++ src/de/kritzelbit/orebit/io/GameIO.java
@@ -10,13 +10,9 @@ import java.util.logging.Logger;
 
 public class GameIO {
     
+    //TODO: CHANGE THIS TO SOMETHING THAT WORKS!!!
     private static final String SAVEGAME_PATH  = System.getProperty("user.home") + "/.OreBit/savegame.sav";
     
-    /**
-     * Saves the given savegame to a pre-defined location, which is
-     * <code>&lt;user home&gt;/.OreBit/savegame.sav</code>
-     * @param saveGame 
-     */
     public static void writeSaveGame(SaveGame saveGame){
         try {
             BinaryExporter.getInstance().save(saveGame, new File(SAVEGAME_PATH));
@@ -25,13 +21,6 @@ public class GameIO {
         }
     }
     
-    /**
-     * Tries to read an existing savegame from the users home directory
-     * and returns the resulting SaveGame object. If no savegame can be found,
-     * a new SaveGame instance, featuring default values for a fresh game,
-     * will be returned.
-     * @return SaveGame SaveGame object
-     */
     public static SaveGame readSaveGame(){
         File f = new File(SAVEGAME_PATH);
         if (f.exists()){
