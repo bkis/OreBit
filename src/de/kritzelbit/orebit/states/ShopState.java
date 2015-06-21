0acdc628f054eb1d7d2c76496e2f7ab8bc377005
kritzelbit <boerge.kiss@gmail.com>
2015-06-21 15:10:53 +0200

    placeholder shopstate

---
+++ src/de/kritzelbit/orebit/states/ShopState.java
@@ -0,0 +1,27 @@
+package de.kritzelbit.orebit.states;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+
+
+public class ShopState extends AbstractAppState {
+    
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        
+        
+    }
+    
+    @Override
+    public void update(float tpf) {
+        
+    }
+    
+    @Override
+    public void cleanup() {
+        super.cleanup();
+        
+    }
+}
